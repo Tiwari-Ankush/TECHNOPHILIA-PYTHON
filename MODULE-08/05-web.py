f803737b86555aeb10f6151f0707da944a690388
Ankush <ankushgt25@gmail.com>
2023-07-26 19:48:40 +0530

    tiwaribro

--- MODULE-08/05-web.py
+++ MODULE-08/05-web.py
@@ -1,13 +1,17 @@
 import requests
 import bs4
+import lxml.etree as xml
 
 url = "https://github.com/requests/requests"
 
 response = requests.get(url)
-# print(response)
+print(response)
 # print(response.text)
 
 web_page = bs4.BeautifulSoup(response.text, "lxml")
+
+# print(web_page)
+print(type(web_page))
 
 # print(web_page.head)
 # print(web_page.head.title)
