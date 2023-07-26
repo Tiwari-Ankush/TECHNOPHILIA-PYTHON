d0e8d6bfcd61822a19753f9207afc7d5056daa9f
Ankush <ankushgt25@gmail.com>
2023-07-26 18:28:05 +0530

    tiwaribro

--- MODULE-08/02-pokemon.py
+++ MODULE-08/02-pokemon.py
@@ -7,9 +7,9 @@ print(response)
 recieved_data = response.json()
 # print(recieved_data)
 
-# import matplotlib.pyplot as plt
-# # url_data = recieved_data["cards"][1]["imageUrl"]
-# # print(url_data)
+import matplotlib.pyplot as plt
+url_data = recieved_data["cards"][1]["imageUrl"]
+print(url_data)
 # url_data = requests.get(recieved_data["cards"][0]["imageUrl"])
 # with open('./poke.png','wb') as f:
 #     for item in url_data.iter_content(4096):
