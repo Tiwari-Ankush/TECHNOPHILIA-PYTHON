3cdd08cd54734370195dccaafd5f6175b0005e2d
Ankush <ankushgt25@gmail.com>
2023-07-26 19:11:54 +0530

    tiwaribro

--- MODULE-08/03-func_pokemon.py
+++ MODULE-08/03-func_pokemon.py
@@ -1,12 +1,16 @@
 import requests
 import matplotlib.pyplot as plt
 
+
+# GETTING DATA 
 def get_pokemon_data():
     poke_name = input("Enter the name of Pokemon: ")
     url = "https://api.pokemontcg.io/v1/cards?name={}".format(poke_name)
     response = requests.get(url)
     return response.json()
 
+
+# PLOTING A DATA 
 def plot_pokemon_image(recieved_data):
     url_data = requests.get(recieved_data["cards"][1]["imageUrl"])
     with open('./MODULE-08/pokemon2.png','wb') as f:
--- MODULE-08/04-certificate.py
+++ MODULE-08/04-certificate.py
@@ -1,26 +1,29 @@
 # https://www.dropbox.com/s/th0u3mpsl71fxit/CertificateTemplate.jpg
 
 from PIL import Image, ImageDraw, ImageFont
-img = Image.open("./CertificateTemplate.jpg")
+img = Image.open("./MODULE-08/Certi_Template.jpg")
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+# image show 
 def print_img(img):
     plt.imshow(np.array(img))
     plt.show()
 
 import cv2
 
-img = cv2.imread("./CertificateTemplate.jpg")
+img = cv2.imread("./MODULE-08/Certi_Template.jpg")
 
 def generate_certificate(img, name="Enter Name"):
-    generated_image = img.copy()
-    font = cv2.FONT_HERSHEY_SIMPLEX
+    generated_image = img.copy()  #COPY THE IMAGE
+    font = cv2.FONT_HERSHEY_SIMPLEX #FONT IN CV2 LIB
+
     cordinates = (700,750)
     font_size = 3.5
     font_color = (51,51,51)
     font_thickness = 10
+    
     cv2.putText(generated_image,name,cordinates,font,font_size,font_color,font_thickness)
     return generated_image
 
