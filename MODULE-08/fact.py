1ffc5e32a740abbeb732edecf24282c6e2fff9e1
Ankush <ankushgt25@gmail.com>
2023-07-26 19:00:46 +0530

    tiwaribro

--- MODULE-08/fact.py
+++ MODULE-08/fact.py
@@ -1,13 +1,17 @@
-# def factorial(number):
-#     fact = 1
-#     if number == 0 or number==1: 
-#         return 1    
-#     else:
-#         for i in range(2,number):
-#             fact*=i   
+def factorial(number):
+    fact = 1
+    if number == 0 or number==1: 
+        return 1    
+    else:
+        for i in range(2,number+1):
+            fact*=i   
+    return fact
+
 
 import math
 
 if __name__ == "__main__":
     num = int(input())
+    print(factorial(num))
+
     print(math.factorial(num))
