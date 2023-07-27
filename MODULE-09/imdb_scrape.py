cb513f7d337f19facc4579c9c05d2c68e1578373
Ankush <ankushgt25@gmail.com>
2023-07-27 15:27:53 +0530

    tiwaribro

--- MODULE-09/imdb_scrape.py
+++ MODULE-09/imdb_scrape.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 def get_movie_ids(num=30, page=1):
-    links_data = pd.read_csv("./scrape_imdb/links.csv")
+    links_data = pd.read_csv("./MODULE-09/links.csv")
     movie_ids = list(links_data.imdbId)
     start_index = (page - 1) * num
     end_index = start_index + num
---
+++ MODULE-09/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+def get_movie_ids(num=20,page=1):
+#     links_data=pd.read_csv("./MODULE-09/links.csv")
+#     movie_ids = list(links_data.imdbId)
+#     start_index=(page-1)*num
+#     end_index = start_index+num
+#     return movie_ids[start_index:end_index]
