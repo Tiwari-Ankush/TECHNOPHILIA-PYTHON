85376c1bd7ad1712f5370733532f192200c7ad92
Ankush <ankushgt25@gmail.com>
2023-07-27 14:10:44 +0530

    tiwaribro

---
+++ MODULE-09/00-readme.txt
@@ -0,0 +1,11 @@
+
+imdb website url >> 
+https://www.imdb.com/
+
+    url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
+    explain this LOC ?
+
+"
+    6 digit id was not avai;able 
+    therefore for making it to 7, we will add 0 in front of id
+    
--- MODULE-09/01-imdb-scrapping.py
+++ MODULE-09/01-imdb-scrapping.py
@@ -6,6 +6,8 @@ IMDB DATA EXTRACTION USING BEAUTIFUL SOUP >>
 
 from bs4 import BeautifulSoup
 import pandas as pd
+import json
+import requests
 
 # df = pd.read_csv("./MODULE-09/links.csv")
 # print(df.head())
@@ -17,6 +19,34 @@ def get_movie_id(num=20,page=1):
     end_index = start_index+num
     return movie_ids[start_index:end_index]
 
+def scrape_movie_index_page(movie_id):
+    url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
+    current_page =requests.get(url)
+    index_soup = BeautifulSoup(current_page.text,"html.parser")
+    current_page_json= index_soup.find("script",attrs={"type":"application/ld+json"})
+
+    current_page_json=str(current_page_json)
+    current_page_json[str(current_page_json).find("{"):-9]
+    
+    return current_page_json
+    # print(current_page_json)
+    
+
+def collect_movie_data(movie_id):
+    page_json = json.loads(scrape_movie_index_page(movie_id))
+    movie= {
+        "name": page_json["name"],
+        "genre":page_json["genre"],
+        "image":page_json["image"],
+        "description":page_json["description"]
+    }
+    print(movie["name"])
+    return movie
+
+
 if __name__== "__main__":
-    ids= get_movie_id(10)
-    print(ids)
+    ids= get_movie_id(20)
+    # print(ids)
+
+    for x in ids:
+        collect_movie_data(1)
