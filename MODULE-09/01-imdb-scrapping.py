8410852ee9ef6fa6ccd8c7948e3a1a72ebf74931
Ankush <ankushgt25@gmail.com>
2023-07-27 13:40:55 +0530

    tiwaribro

--- MODULE-09/01-imdb-scrapping.py
+++ MODULE-09/01-imdb-scrapping.py
@@ -1,5 +1,22 @@
+
+print('''
+
+IMDB DATA EXTRACTION USING BEAUTIFUL SOUP >>
+''')
+
 from bs4 import BeautifulSoup
 import pandas as pd
 
-df = pd.read_csv("./MODULE-09/links.csv")
-print(df.head())
+# df = pd.read_csv("./MODULE-09/links.csv")
+# print(df.head())
+
+def get_movie_id(num=20,page=1):
+    links_data=pd.read_csv("./MODULE-09/links.csv")
+    movie_ids = list(links_data.imdbId)
+    start_index=(page-1)*num
+    end_index = start_index+num
+    return movie_ids[start_index:end_index]
+
+if __name__== "__main__":
+    ids= get_movie_id(10)
+    print(ids)
